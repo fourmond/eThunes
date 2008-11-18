c6902570b8b54a395b6aac8836a33277e703cb27
Vincent Fourmond <vincent.fourmond@9online.fr>
2008-11-18 21:20:52 +0100

    Setting up the first files

---
+++ QMoney.pro
@@ -0,0 +1,19 @@
+# -*- mode: conf; -*-
+# QMake configuration file for QCam
+
+TEMPLATE = app
+CONFIG += precompile_header debug
+INCLUDEPATH += . src
+
+VERSION = 0.1
+
+QT += sql
+
+PRECOMPILED_HEADER = src/headers.hh
+
+# Following lines for profiling
+# QMAKE_CXXFLAGS += -pg
+# QMAKE_LFLAGS += -pg
+
+# Input files
+SOURCES += src/qmain.cc 
---
+++ src/headers.hh
@@ -0,0 +1,31 @@
+/** 
+    \file headers.hh precompiled headers for QMoney
+    Copyright 2008 by Vincent Fourmond
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __HEADERS_HH
+#define __HEADERS_HH
+
+#include <QApplication>
+#include <QMainWindow>
+
+// SQL-related stuff:
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlResult>
+
+#endif
