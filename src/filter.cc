1a0f6ef5bc82b19c5c360f4d1e69fb8c88722547
Vincent Fourmond <vincent.fourmond@9online.fr>
2010-03-31 00:46:31 +0200

    First implementation of filters, though untested yet

---
+++ src/filter.cc
@@ -0,0 +1,90 @@
+/*
+    filter.cc: The Filter class
+    Copyright 2010 by Vincent Fourmond
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <headers.hh>
+#include <filter.hh>
+
+SerializationAccessor * FilterElement::serializationAccessor()
+{
+  SerializationAccessor * ac = new SerializationAccessor(this);
+  ac->addAttribute("attribute", 
+		   new SerializationItemScalar<int>((int*)&transactionAttribute, true));
+  ac->addAttribute("regexp", 
+		   new SerializationItemScalar<bool>(&regexp, true));
+  ac->addAttribute("match", 
+		   new SerializationItemScalar<QString>(&match));
+  return ac;
+
+}
+
+bool FilterElement::matches(Transaction * t)
+{
+  QString targetString;
+  switch(transactionAttribute) {
+  case Name: targetString = t->name;break;
+  case Memo: targetString = t->memo;break;
+  }
+  
+  if(regexp)
+    return targetString.contains(QRegExp(match));
+  else
+    return targetString.contains(match);
+}
+
+Filter::Filter() : active(true), matchAny(true) 
+{
+}
+
+SerializationAccessor * Filter::serializationAccessor()
+{
+  SerializationAccessor * ac = new SerializationAccessor(this);
+  ac->addAttribute("name", 
+		   new SerializationItemScalar<QString>(&name, true));
+  ac->addAttribute("category", 
+		   new SerializationItemScalar<QString>(&category, true));
+  ac->addAttribute("matchany", 
+		   new SerializationItemScalar<bool>(&matchAny, true));
+  ac->addAttribute("active", 
+		   new SerializationItemScalar<bool>(&active, true));
+  ac->addAttribute("elements", 
+		   new SerializationQList<FilterElement>(&elements));
+  return ac;
+
+}
+
+bool Filter::matches(Transaction * t)
+{
+  for(int i = 0; i < elements.size(); i++) {
+    bool ok = elements[i].matches(t);
+    if(matchAny && ok)
+      return true; // OR logic and one match
+    else if(! (matchAny || ok))
+      return false; // AND logic and one non-match
+  }
+  return ! matchAny;
+}
+
+void Filter::processList(TransactionList l)
+{
+  for(int i = 0; i < l.size(); i++) {
+    Transaction * t = &l[i];
+    if(!t->category && matches(t)) // Does not set when category
+				   // exists, please override.
+      t->setCategoryFromName(category);
+  }
+}
---
+++ src/filter.hh
@@ -0,0 +1,84 @@
+/** 
+    \file filter.hh
+    The Filter class
+    Copyright 2010 by Vincent Fourmond
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __FILTER_HH
+#define __FILTER_HH
+
+#include <serializable.hh>
+#include <transaction.hh>
+
+/// This is a simple filter element, that applies to a single
+/// attribute of a target Transaction, and returns yes/no.
+class FilterElement : public Serializable {
+public:
+  virtual SerializationAccessor * serializationAccessor();
+
+  /// The target string in the Transaction to test
+  enum {
+    Name,
+    Memo
+  } transactionAttribute;
+  
+  /// The string to match it against.
+  QString match;
+
+  /// Regular expression ?
+  bool regexp;
+
+  /// Whether the element matches a target transaction or not.
+  bool matches(Transaction * t);
+
+  FilterElement() : transactionAttribute(Name), regexp(false) {;};
+};
+
+/// This class represents a simple filter for setting the categories.
+///
+/// \todo when tags are implemented, this should handle tags too.
+class Filter : public Serializable {
+public:
+
+  /// Name of the filter
+  QString name;
+
+  /// Whether it is active or not
+  bool active;
+
+  /// List of the elements of the filter
+  QList<FilterElement> elements;
+
+  /// Should the Filter match all (false) or any (true) element ?
+  bool matchAny;
+
+  /// The target Category
+  QString category;
+
+  /// Implementation of the Serialization accessor
+  virtual SerializationAccessor * serializationAccessor();
+
+  Filter();
+
+  /// Whether or not
+  bool matches(Transaction * t);
+
+  /// Loops over the list and sets the categories according to the tests.
+  void processList(TransactionList l);
+};
+
+#endif
