d7c30cef3496fb4a2e5bcd1cbac18822a3708b01
Vincent <vincent.fourmond@9online.fr>
2013-12-29 16:44:43 +0100

    Proper dialog box for prompting for credentials

--- src/identitiessafe.cc
+++ src/identitiessafe.cc
@@ -25,11 +25,70 @@ IdentitiesSafe::~IdentitiesSafe()
   // Nothing to destroy, it seems...
 }
 
+class PasswordInput : public QDialog {
+  QLineEdit * login;
+
+  QLineEdit * password;
+public:
+
+  /// Returns the login
+  QString getLogin() const {
+    return login->text();
+  };
+
+
+  /// Returns the password
+  QString getPassword() const {
+    return password->text();
+  };
+
+  PasswordInput(const QString & title, const QString & label) {
+    setWindowTitle(title);
+    QVBoxLayout * layout = new QVBoxLayout(this);
+    layout->addWidget(new QLabel(label));
+
+    QHBoxLayout * hb;
+
+    hb = new QHBoxLayout;
+    hb->addWidget(new QLabel(tr("User name")));
+    login = new QLineEdit();
+    hb->addWidget(login);
+    layout->addLayout(hb);
+
+    hb = new QHBoxLayout;
+    hb->addWidget(new QLabel(tr("Password")));
+    password = new QLineEdit();
+    password->setEchoMode(QLineEdit::Password);
+    hb->addWidget(password);
+    layout->addLayout(hb);
+
+    QPushButton * bt;
+    hb = new QHBoxLayout;
+
+    bt = new QPushButton(tr("OK"));
+    connect(bt, SIGNAL(clicked()), SLOT(accept()));
+    hb->addWidget(bt);
+
+    bt = new QPushButton(tr("Cancel"));
+    connect(bt, SIGNAL(clicked()), SLOT(reject()));
+    hb->addWidget(bt);
+
+    layout->addLayout(hb);
+  };
+};
+
 AttributeHash IdentitiesSafe::getCredentialsForCollection(Collection * collection)
 {
   AttributeHash user;
-  user["login"] = QInputDialog::getText(NULL, "login", "login");
-  user["passwd"] = QInputDialog::getText(NULL, "passwd", "passwd",
-                                         QLineEdit::Password);
+  
+  PasswordInput input(QString("Credentials for %1").arg(collection->name),
+                      QString("Enter credentials for collection %1").
+                      arg(collection->name));
+  if(input.exec() != QDialog::Accepted)
+    throw "Stuff";
+
+  user["login"] = input.getLogin();
+  user["passwd"] = input.getPassword();
+
   return user;
 }
