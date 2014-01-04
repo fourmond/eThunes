04792eed95e7c9289fac7be403356812c901fa45
Vincent <vincent.fourmond@9online.fr>
2014-01-04 17:59:59 +0100

    [collections] convert the old H&K collection to Ruby

---
+++ collections/h-k.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+CollectionDefinition.collection('h-k') do
+
+  public_name "H&K"
+  description <<EOD
+???
+EOD
+  
+  document "DA" do 
+    display "Droits d'auteur %{year}: %{amount%A} (date: %{date%date:d/MM/yyyy})"
+    format "H&K/DA-%{year}.pdf"
+    public_name "Droits d'auteur"
+
+    def parse(doc)
+      res = {}
+      if doc['text-layout'] =~ /(\d+)(?:er)?\s*(#{Dates::FrenchMonthRE})\s*(\d+)/
+        year = $3
+        res['date'] = Time.local(year.to_i, Dates::parse_french_month($2),$1.to_i)
+        res['year'] = year.to_i - 1
+      end
+      if doc['text-layout'] =~ /Droit\s+net.*#{year}(?:\s*=)?\s+(\d+)\s*[.,]\s*(\d+)\s*e/
+        res['amount'] = $1.to_i * 100 + $2.to_i
+      end
+      return res
+    end
+  end
+end
